49299f4d40fd359f38611e2084102f9ea8a17092
Ash <sgt16@yandex.ru>
2021-06-18 00:06:29 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/utils/Post.java
@@ -0,0 +1,54 @@
+package ru.job4j.grabber.utils;
+
+public class Post {
+    private int id;
+    private String name;
+    private String description;
+    private String link;
+    private String date;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Post setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public Post setLink(String link) {
+        this.link = link;
+        return this;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Post setDate(String date) {
+        this.date = date;
+        return this;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Post setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Post setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
